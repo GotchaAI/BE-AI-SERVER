4a6d44bdd56fc1ffb16c63c3eee748b84818bb9b
brothergiven <dung3740@gmail.com>
2025-03-18 14:27:37 +0900

    feat: CNN model 예측 처리 코드 추가

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,2 +1,10 @@
 # handle trained model(CNN)
+import config
+import os
+from tensorflow import keras
 
+print("모델 로딩 중...")
+print("현재 경로:", os.getcwd())  # 현재 실행 중인 Python 스크립트의 경로 확인
+print("모델 경로:", os.path.abspath(config.MODEL_PATH))  # 모델 경로 확인인
+model = keras.models.load_model(config.MODEL_PATH)
+print("모델 로드 완료!")
