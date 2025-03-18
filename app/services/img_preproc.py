1438071d30c012af3ab4286a030821c08c7f9c9c
brothergiven <dung3740@gmail.com>
2025-03-18 12:39:48 +0900

    feat: 이미지 전처리 함수

---
+++ app/services/img_preproc.py
@@ -0,0 +1,26 @@
+import config
+import numpy as np
+from text_masking import recog_image, mask_image
+from PIL import Image
+
+def preproc(image_bytes: bytes, image_size=config.IMAGE_SIZE):
+    """
+    이미지 전처리 함수
+    1. text masking 수행
+    2. 흑백 변환
+    3. 크기 조정
+    4. 정규화
+    5. 차원 확장
+    """
+    masking_box = recog_image(image_bytes=image_bytes)
+    masked_img = mask_image(image_bytes=image_bytes, boxes=masking_box) # PIL
+
+    img = masked_img.resize(image_size)
+
+    img_arr = np.array(img) / 255.0
+    img_arr = np.expand_dims(img_arr, axis=-1)
+    img_arr = np.expand_dims(img_arr, axis=0)
+
+    return img_arr
+
+
--- config.py
+++ config.py
@@ -5,4 +5,9 @@ LOW_TEXT=0.4
 CUDA=False
 LONG_SIZE=1280
 
-MODEL_PATH='data/model'
+
+MODEL_PATH='./data/model'
+
+# QuickDraw 데이터 설정
+MAX_DRAWINGS = 10000  # 각 클래스(고양이, 개)별 샘플 개수
+IMAGE_SIZE = (28, 28)  # 이미지 크기 (너비, 높이)
