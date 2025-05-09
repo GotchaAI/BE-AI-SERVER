946ab795aebbc85386faea9c7cb9a51f6002d5e6
brothergiven <128305393+brothergiven@users.noreply.github.com>
2025-05-10 02:20:30 +0900

    refact: OCR 패키지 변경, torch 버전 변경 (#3)

--- src/services/img_preproc.py
+++ src/services/img_preproc.py
@@ -1,7 +1,6 @@
 import io
 from PIL import Image
-from src.services.text_masking import recog_text, mask_text
-import matplotlib.pyplot as plt
+from src.services.text_masking import mask_text
 
 def preproc(image_bytes: bytes):
     """
@@ -9,18 +8,6 @@ def preproc(image_bytes: bytes):
     1. PIL.Image로 변환
     2. 텍스트 검출 후 masking
     """
-    masking_box = recog_text(image_bytes)
     image = Image.open(io.BytesIO(image_bytes)).convert('RGB')
-    masked_img = mask_text(image, masking_box) # PIL
+    masked_img = mask_text(image)
     return masked_img
-
-
-def show_image(image):
-    """
-    Args:
-        image (PIL.Image): 출력할 이미지
-    """
-    plt.figure(figsize=(10, 6))
-    plt.imshow(image)
-    plt.axis("off")
-    plt.show()
