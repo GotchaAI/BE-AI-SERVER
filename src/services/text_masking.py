447bb9973162d13e5f07ade500e9c93ddcec00d0
brothergiven <128305393+brothergiven@users.noreply.github.com>
2025-05-10 02:22:02 +0900

    refact: OCR 패키지 변경, torch 버전 변경 (#3) (#4)

--- src/services/text_masking.py
+++ src/services/text_masking.py
@@ -1,49 +1,35 @@
 import io
 from PIL import Image, ImageDraw
 import config
-from craft_text_detector import (
-    load_craftnet_model,
-    load_refinenet_model,
-    get_prediction
-)
-import logging
+import easyocr
+import numpy as np
 
-logging.info("CRAFT_NET 가중치 로딩중 ...")
-craft_net = load_craftnet_model(cuda=config.CUDA)
-logging.info("CRAFT_NET 가중치 로딩 완료.")
+reader = easyocr.Reader(['en', 'ko'])
 
-logging.info("REFINE_NET 가중치 로딩중...")
-refine_net = load_refinenet_model(cuda=config.CUDA)
-logging.info("REFINE_NET 가중치 로딩 완료.")
-
-
-def recog_text(image: bytes):
+def recog_text(image: Image):
     """
     Args: image
     Returns: 이미지가 인식된 바운딩 박스
     """
-    logging.info("텍스트 검출 중...")
-    prediction_res = get_prediction(
-        image = image,
-        craft_net=craft_net,
-        refine_net=refine_net,
-        text_threshold=config.TEXT_THRESHOLD,
-        link_threshold=config.LINK_THRESHOLD,
-        low_text=config.LOW_TEXT,
-        cuda=config.CUDA,
-        long_size=config.LONG_SIZE
-    )
-    logging.info('텍스트 검출 완료.')
-    logging.info(f'텍스트 검출 걸린 시간: {sum(prediction_res["times"].values()):.2f}초.')
-    return prediction_res['boxes']
+    image_np = np.array(image)
+
+    results = reader.readtext(image_np)
+
+    filtered_boxes = [box for box, text, conf in results if conf >= config.TEXT_THRESHOLD]
+
+    for i, box in enumerate(filtered_boxes):
+        print(f"[{i + 1}] 박스 좌표 (신뢰도 ≥ {config.TEXT_THRESHOLD}): {box}")
+
+    return filtered_boxes
 
 
 
-def mask_text(image: Image, boxes):
+def mask_text(image: Image):
     """
     Args: PIL Image(RGB), 바운딩 박스
     Returns: 마스킹 된 이미지 데이터
     """
+    boxes = recog_text(image)
     masked = image.copy()
     draw = ImageDraw.Draw(masked)
     for box in boxes:
