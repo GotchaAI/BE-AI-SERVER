2c58194b567f2953a092546831700eb2f136eb2e
brothergiven <dung3740@gmail.com>
2025-09-19 19:09:40 +0900

    refact: captioning 코드 재작성

--- src/core/caption.py
+++ src/core/caption.py
@@ -1,19 +1,33 @@
 from transformers import BlipProcessor, BlipForConditionalGeneration
 from PIL import Image
+from io import BytesIO
 import torch
-
+from src.config import settings
 
 print('BLIP 모델 로딩중....')
-processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
-model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
+processor = BlipProcessor.from_pretrained(settings.CAPTIONING_MODEL)
+model = BlipForConditionalGeneration.from_pretrained(settings.CAPTIONING_MODEL)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
 print('BLIP 모델 로딩완료!')
 
 
 
-def get_caption(image: Image) -> str:
-    inputs = processor(images=image, return_tensors="pt")
+def generate_caption(image: bytes) -> str:
+    """
+    이미지 캡션 생성 함수
+
+    Args:
+        image (bytes): 이미지 바이트 데이터
+
+    Returns:
+        str: 생성된 캡션
+    """
+    image = Image.open(BytesIO(image)).convert("RGB")
+    inputs = processor(image, return_tensors="pt").to(device)
 
     with torch.no_grad():
-        output_ids = model.generate(**inputs)
-    caption = processor.decode(output_ids[0], skip_special_tokens=True)
+        out = model.generate(**inputs)
+
+    caption = processor.decode(out[0], skip_special_tokens=True)
     return caption
